063a34475dab83312bf885e538124ea03951c516
Jason Pellerin <pellerinj5@gmail.com>
2015-03-15 23:02:35 -0700

    Update btc_tickr.ino

--- btc_tickr.ino
+++ btc_tickr.ino
@@ -1,4 +1,19 @@
 // btc_tickr.ino
+The circuit:
+* LCD RS pin to digital pin 8
+* LCD Enable pin to digital pin 6
+* LCD D4 pin to digital pin 5
+* LCD D5 pin to digital pin 4
+* LCD D6 pin to digital pin 3
+* LCD D7 pin to digital pin 2
+* LCD R/W pin to ground
+* LCD pin 16 to digital pin 11
+* LCD pin 17 to digital pin 10
+* LCD pin 18 to digital pin 9
+* 10K resistor:
+* ends to +5V and ground
+* wiper to LCD VO pin (pin 3)
+*/
 #include <LiquidCrystal.h>
 
 // Initialize lcd display
